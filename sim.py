7b8742011fd933e191f75bafddcc7df62ef4420c
Nicholas Schweitzer <ns__>
2021-08-09 13:44:27 +0200

    Switch to MacCormack convection

--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse.linalg
 import matplotlib.pyplot as plt
 import matplotlib.widgets as widgets
 import common
@@ -17,13 +18,13 @@ class Grid:
                 self.width = width
                 self.height = height
                 self.pressure = np.zeros((height, width))
-                self.u = np.zeros((height, width+1))
-                self.v = np.zeros((height+1, width))
+                self.u = np.zeros((height+2, width+1))
+                self.v = np.zeros((height+1, width+2))
 
                 # Generate positions for velocity fields, used for plotting them at grid
                 # edges rather than centers
-                self.u_meshes = np.meshgrid(np.arange(0, self.width+1, 1), np.arange(0.5, self.height + 0.5, 1))
-                self.v_meshes = np.meshgrid(np.arange(0.5, self.width+0.5, 1), np.arange(0, self.height + 1, 1))
+                self.u_meshes = np.meshgrid(np.arange(0, self.width+1, 1), np.arange(-0.5, self.height + 1.5, 1))
+                self.v_meshes = np.meshgrid(np.arange(-0.5, self.width+1.5, 1), np.arange(0, self.height + 1, 1))
 
                 # Also generate positions for the averaged velocity fields, where velocities are at grid cell centers
                 self.avg_meshes = np.meshgrid(np.arange(0.5, self.width+0.5, 1), np.arange(0.5, self.height+0.5, 1))
@@ -88,91 +89,58 @@ class Grid:
                 axes.set_yticks(y_ticks)
                 plt.grid()
 
-        def interpolate(self, x, y, idx):
-                field = (self.u, self.v)[idx]
-
-                # U is offset vertically by 0.5
-                if idx == 0: y -= 0.5
-                # V is offset horizontally by 0.5
-
-                elif idx == 1: x -= 0.5
-                # Lowest x and y
-                lx, ly = math.floor(x), math.floor(y)
-
-                # The bounds depend on which field is being interpolated: U and V each have an extra cell in each direction (horizontal/vertical)
-                max_width, max_height = self.width, self.height
-                if idx == 0: max_width += 1
-                elif idx == 1: max_height += 1
-
-                if lx < 0 or ly < 0 or lx+1 == max_width or ly+1 == max_height:
-                        raise ValueError(f'x and y out of bounds: ({x}, {y})')
-
-                bottom_left = field[ly, lx]
-                bottom_right = field[ly, lx+1]
-                top_left = field[ly+1, lx]
-                top_right = field[ly+1, lx+1]
-
-                dist_left = x - lx
-                dist_right = lx+1 - x
-                dist_bottom = y - ly
-                dist_top = ly+1 - y
-
-                return bottom_left * (1 - dist_left) * (1 - dist_bottom) \
-                        + bottom_right * (1 - dist_right) * (1 - dist_bottom) \
-                        + top_left * (1 - dist_left) * (1 - dist_top) \
-                        + top_right * (1 - dist_right) * (1 - dist_top)
-
         def apply_bcs(self):
                 # Left
                 self.u[:,0] = -self.u[:,1]
                 # Right
                 self.u[:,-1] = -self.u[:,-2]
                 # Bottom
-                self.u[0,:] = 0
+                self.u[0:1,:] = 0
                 # Top
-                self.u[-1,:] = 10
+                self.u[-2:-1,:] = 10
 
                 # Left
-                self.v[:,0] = 0
+                self.v[:,0:1] = 0
                 # Right
-                self.v[:,-1] = 0
+                self.v[:,-2:1] = 0
                 # Bottom
                 self.v[0,:] = -self.v[1,:]
                 # Top
                 self.v[-1,:] = -self.v[-2,:]
 
         def apply_convection(self, dt):
-                new_u = self.u.copy()
-                new_v = self.v.copy()
+                dx, dy = 1, 1
+                v_at_u = (self.v[1:,1:-2] + self.v[1:,2:-1] + self.v[:-1,1:-2] + self.v[:-1,2:-1]) / 4
+                u_at_v = (self.u[1:-2,1:] + self.u[2:-1,1:] + self.u[1:-2,:-1] + self.u[2:-1,:-1]) / 4
 
-                # Only convect inside the boundary. U has an extra cell horizontally
-                for y in range(1, self.height-1):
-                        for x in range(1, self.width):
-                                # Get new velocity for u, offset by 0.5 vertically
-                                new_u[y,x] = self.rk2_trace(x, y + 0.5, dt, 0)
+                # Predict
+                predicted_u = self.u.copy()
+                u_diff_x_forward = (self.u[1:-1,2:] - self.u[1:-1,1:-1]) / dx
+                u_diff_y_forward = (self.u[2:,1:-1] - self.u[1:-1,1:-1]) / dy
+                predicted_u[1:-1,1:-1] += -dt * (self.u[1:-1,1:-1] * u_diff_x_forward + v_at_u * u_diff_y_forward)
 
-                # V has an extra cell vertically
-                for y in range(1, self.height):
-                        for x in range(1, self.width-1):
-                                # Get new velocity at v, offset by 0.5 horizontally
-                                new_v[y,x] = self.rk2_trace(x + 0.5, y, dt, 1)
+                predicted_v = self.v.copy()
+                v_diff_x_forward = (self.v[1:-1,2:] - self.v[1:-1,1:-1]) / dx
+                v_diff_y_forward = (self.v[2:,1:-1] - self.v[1:-1,1:-1]) / dy
+                predicted_v[1:-1,1:-1] += -dt * (u_at_v * v_diff_x_forward + self.v[1:-1,1:-1] * v_diff_y_forward)
 
-                self.u = new_u
-                self.v = new_v
+                # Correct
+                u_corrected = (self.u + predicted_u) / 2
+                u_pred_diff_x_backward = (predicted_u[1:-1,1:-1] - predicted_u[1:-1,:-2]) / dx
+                u_pred_diff_y_backward = (predicted_u[1:-1,1:-1] - predicted_u[:-2,1:-1]) / dy
+                u_corrected[1:-1,1:-1] += -0.5 * dt * (self.u[1:-1,1:-1] * u_pred_diff_x_backward + v_at_u * u_pred_diff_y_backward)
 
-        def rk2_trace(self, x, y, dt, idx):
-                # Order-two Runge-Kutta interpolation: take half an Euler step and use the velocity there as an average over the whole step
-                half_x = x - self.interpolate(x, y, 0) * 0.5 * dt
-                half_y = y - self.interpolate(x, y, 1) * 0.5 * dt
-                half_vel = (self.interpolate(half_x, half_y, 0), self.interpolate(half_x, half_y, 1))
+                v_corrected = (self.v + predicted_v) / 2
+                v_pred_diff_x_backward = (predicted_v[1:-1,1:-1] - predicted_v[1:-1,:-2]) / dx
+                v_pred_diff_y_backward = (predicted_v[1:-1,1:-1] - predicted_v[:-2,1:-1]) / dy
+                v_corrected[1:-1,1:-1] += -0.5 * dt * (u_at_v * v_pred_diff_x_backward + self.v[1:-1,1:-1] * v_pred_diff_y_backward)
 
-                final_x = x - half_vel[0] * dt
-                final_y = y - half_vel[1] * dt
-                return self.interpolate(final_x, final_y, idx)
+                self.u[1:-1,1:-1] = u_corrected[1:-1,1:-1]
+                self.v[1:-1,1:-1] = v_corrected[1:-1,1:-1]
 
         def average_velocities(self):
-                u_avg = (self.u[:,:-1] + self.u[:,1:]) / 2
-                v_avg = (self.v[:-1,:] + self.v[1:,:]) / 2
+                u_avg = (self.u[1:-1,:-1] + self.u[1:-1,1:]) / 2
+                v_avg = (self.v[:-1,1:-1] + self.v[1:,1:-1]) / 2
                 return (u_avg, v_avg)
 
         def apply_viscosity(self, dt, nu):
@@ -188,19 +156,19 @@ class Grid:
 
         def apply_pressure(self, dt, rho):
                 # Solve for pressure
-                u_diff_x = self.u[:,1:] - self.u[:,:-1]
-                v_diff_y = self.v[1:,:] - self.v[:-1,:]
+                u_diff_x = self.u[1:-1,1:] - self.u[1:-1,:-1]
+                v_diff_y = self.v[1:,1:-1] - self.v[:-1,1:-1]
                 divergence = u_diff_x + v_diff_y
 
                 pressure_rhs = rho / dt * divergence
                 pressure_rhs_flat = np.reshape(pressure_rhs, (self.width*self.height))
 
-                pressure_flat = np.linalg.solve(self.pressure_coef, pressure_rhs_flat)
+                pressure_flat = scipy.sparse.linalg.spsolve(self.pressure_coef, pressure_rhs_flat)
                 self.pressure = np.reshape(pressure_flat, (self.width, self.height))
 
                 # Apply pressure
-                p_diff_x = self.pressure[1:-1,1:] - self.pressure[1:-1,:-1]
-                p_diff_y = self.pressure[1:,1:-1] - self.pressure[:-1,1:-1]
+                p_diff_x = self.pressure[:,1:] - self.pressure[:,:-1]
+                p_diff_y = self.pressure[1:,:] - self.pressure[:-1,:]
 
                 self.u[1:-1,1:-1] += -(dt / rho) * p_diff_x
                 self.v[1:-1,1:-1] += -(dt / rho) * p_diff_y
@@ -231,7 +199,23 @@ def click_callback(grid, event):
 
 plt.connect('button_press_event', lambda event: click_callback(grid, event))
 
-grid = Grid(20, 20)
+grid = Grid(60, 60)
 grid.plot()
 
-plt.show()
+ct = 0
+dt, nu, rho = 0.05, 0.1, 1.0
+while 1:
+        plt.clf()
+        grid.plot()
+
+        plt.savefig(f'img/{ct:04}.png', dpi=600)
+        print(ct)
+
+        for i in range(100):
+                grid.apply_bcs()
+                grid.apply_convection(dt)
+                grid.apply_viscosity(dt, nu)
+                grid.apply_pressure(dt, rho)
+                print(f'\t{i}')
+
+        ct += 1
